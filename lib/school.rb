2a293167b721e7a8aa3ffa9c5a260a78dda12905
Alexander Marusin <amarusin18@gmail.com>
2020-07-26 18:39:37 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School
+attr_accessor :grade, :roster
+attr_reader :name
+
+  def initialize(name)
+      @name = name
+      @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    array = []
+  if @roster == nil
+    @roster[grade]
+  else
+    @roster[grade] = array << name
+  end
+  end
+end
